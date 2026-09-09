816a2f0182bae3756f00e68fbe0b82ae75cf7fda
Pavel Akulov <pavel.akulov.88@gmail.com>
2026-09-09 03:16:15 -0400

    CarMake, CarModel

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,11 +6,12 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
     path(route='register', view=views.registration, name='registration'),
 
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
+
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
